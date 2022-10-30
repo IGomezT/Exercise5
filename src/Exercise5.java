e72bd1ad194aa93ffd05d2f0824571a62ac63181
Maykel2 <alugom1966@ieselcaminas.org>
2022-10-30 18:19:16 +0100

    Initial commit

---
+++ src/Exercise5.java
@@ -0,0 +1,33 @@
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        int[] storage;
+
+        storage = new int[11];
+
+        storage [0] = 10;
+        storage [1] = 9;
+        storage [2] = 8;
+        storage [3] = 7;
+        storage [4] = 6;
+        storage [5] = 5;
+        storage [6] = 4;
+        storage [7] = 3;
+        storage [8] = 2;
+        storage [9] = 1;
+        storage [10] = 0;
+
+        System.out.println("0 to 10 reverse order : " +storage[0]);
+        System.out.println("0 to 10 reverse order : " +storage[1]);
+        System.out.println("0 to 10 reverse order : " +storage[2]);
+        System.out.println("0 to 10 reverse order : " +storage[3]);
+        System.out.println("0 to 10 reverse order : " +storage[4]);
+        System.out.println("0 to 10 reverse order : " +storage[5]);
+        System.out.println("0 to 10 reverse order : " +storage[6]);
+        System.out.println("0 to 10 reverse order : " +storage[7]);
+        System.out.println("0 to 10 reverse order : " +storage[8]);
+        System.out.println("0 to 10 reverse order : " +storage[9]);
+        System.out.println("0 to 10 reverse order : " +storage[10]);
+
+    }
+}
